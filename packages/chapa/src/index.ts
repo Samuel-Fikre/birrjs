3586a64b5d201457b79766300b521f988402c1b1
Samuel Fikre <samicreed12@gmail.com>
2026-04-17 19:58:00 +0300

    refactor: clean up types

--- packages/chapa/src/index.ts
+++ packages/chapa/src/index.ts
@@ -2,3 +2,10 @@ export { chapa } from "./chapa-provider";
 export { createChapaProvider } from "./chapa-provider";
 export { isChapaWebhookEvent } from "./validation";
 export type { ChapaProviderConfig } from "./chapa-provider";
+export type {
+  ChapaTransactionRequest,
+  ChapaTransactionResponse,
+  ChapaVerifyResponse,
+  ChapaWebhookEvent,
+  ChapaTransactionStatus,
+} from "./types";
